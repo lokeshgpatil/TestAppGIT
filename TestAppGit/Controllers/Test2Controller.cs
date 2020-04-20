86dff6358731460ecb21b391fc602cd50a65ffeb
Lokesh Patil <lokeshgpatil@gmail.com>
2020-04-20 13:12:28 +0530

    Test2 added

---
+++ TestAppGit/Controllers/Test2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAppGit.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Test2
+        public ActionResult Index()
+        {
+            //Radhe
+            return View();
+        }
+    }
+}
